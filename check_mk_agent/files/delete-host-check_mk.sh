d5faef1198ed7558c4c3c9c185876d0a2de13346
Tobias Steinhoff <tobias@stonehope.de>
2016-05-17 07:42:17 +0200

    allow foreign changes

--- check_mk_agent/files/delete-host-check_mk.sh
+++ check_mk_agent/files/delete-host-check_mk.sh
@@ -12,4 +12,4 @@
 ### END INIT INFO
 
 curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=delete_host&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}" -d 'request={"hostname": "{{ grains['fqdn'] }}"}'
-curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=activate_changes&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}" 
+curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=activate_changes&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}&allow_foreign_changes=1" 
