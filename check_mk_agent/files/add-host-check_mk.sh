240c070fff70214fd540df5d4d3b245da9c91542
Tobias Steinhoff <t.steinhoff@ak-neuss.de>
2016-09-02 14:16:35 +0200

    get reliable the webtrustip

--- check_mk_agent/files/add-host-check_mk.sh
+++ check_mk_agent/files/add-host-check_mk.sh
@@ -20,9 +20,9 @@
 # Description:       add host to check_mk on boot
 #              
 ### END INIT INFO
-
+{%- set webtrustip = salt.network.ip_addrs(cidr='192.168.10.0/24')[0] %}
 touch /etc/check_mk/firstrun
 
-curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=add_host&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}" -d 'request={"attributes": {"ipaddress": "{{ salt['grains.get']('ipv4')[1] }}","tag_agent": "cmk-agent", "tag_criticality": "{{ salt['pillar.get']('check_mk_agent:criticality') }}"}, "folder": "{{ salt['pillar.get']('check_mk_agent:folder') }}", "hostname": "{{ grains['fqdn'] }}" }'
+curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=add_host&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}" -d 'request={"attributes": {"ipaddress": "{{ webtrustip }}","tag_agent": "cmk-agent", "tag_criticality": "{{ salt['pillar.get']('check_mk_agent:criticality') }}"}, "folder": "{{ salt['pillar.get']('check_mk_agent:folder') }}", "hostname": "{{ grains['fqdn'] }}" }'
 curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=discover_services&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}&mode=fixall" -d 'request={"hostname": "{{ grains['fqdn'] }}"}'
 curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=activate_changes&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}&allow_foreign_changes=1" 
