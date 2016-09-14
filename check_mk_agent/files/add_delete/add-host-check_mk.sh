19fd520f1e0ca230c498cdb2fa775edffcd9073c
Tobias Steinhoff <t.steinhoff@ak-neuss.de>
2016-09-14 12:24:32 +0200

    missing endif

--- check_mk_agent/files/add_delete/add-host-check_mk.sh
+++ check_mk_agent/files/add_delete/add-host-check_mk.sh
@@ -3,7 +3,9 @@
 {%- set webtrustip = salt['pillar.get']('check_mk_agent:hostip') %}
 {% else %}
 {%- set webtrustip = salt.network.ip_addrs(cidr='192.168.10.0/24')[0] %}
+{% endif %}
 touch /etc/check_mk/firstrun
+
 
 curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=add_host&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}" -d 'request={"attributes": {"ipaddress": "{{ webtrustip }}","tag_agent": "cmk-agent", "tag_criticality": "{{ salt['pillar.get']('check_mk_agent:criticality') }}"}, "folder": "{{ salt['pillar.get']('check_mk_agent:folder') }}", "hostname": "{{ grains['fqdn'] }}" }'
 curl "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=discover_services&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}&mode=fixall" -d 'request={"hostname": "{{ grains['fqdn'] }}"}'
