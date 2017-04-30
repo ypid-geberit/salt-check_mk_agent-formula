014af6cc1bc986fabc3e304416d9fdab5dc27dd9
Tobias Steinhoff <tobias@stonehope.de>
2017-04-30 15:19:45 +0200

    use https for api

--- check_mk_agent/files/add_delete/delete-host-check_mk.sh
+++ check_mk_agent/files/add_delete/delete-host-check_mk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -sS "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=delete_host&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}" -d 'request={"hostname": "{{ grains['host'] }}"}'
-curl -sS "http://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=activate_changes&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}&allow_foreign_changes=1"
+curl -sS "https://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=delete_host&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}" -d 'request={"hostname": "{{ grains['host'] }}"}'
+curl -sS "https://{{ salt['pillar.get']('check_mk_agent:server') }}/{{ salt['pillar.get']('check_mk_agent:site') }}/check_mk/webapi.py?action=activate_changes&_username={{ salt['pillar.get']('check_mk_agent:api-user') }}&_secret={{ salt['pillar.get']('check_mk_agent:api-secret') }}&allow_foreign_changes=1"
