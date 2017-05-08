b0a6147a775cba55bb2ab0ec292658bcc457c8d0
Tobias Steinhoff <tobias@stonehope.de>
2017-05-08 07:40:34 +0200

    set higher scur limits

--- check_mk_agent/files/plugins/haproxychecks.py
+++ check_mk_agent/files/plugins/haproxychecks.py
@@ -1,7 +1,7 @@
 # Example checks for the HAProxy plugin
 checks = [
     #fieldname, warning, critical
-    ['scur', '250', '500'],
+    ['scur', '2000', '3000'],
     ['chkfail', '15', '25'],
     ['status', '', ''] # status stays at the end, just for formatting purposes
 ]
